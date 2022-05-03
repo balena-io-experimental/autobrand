84b03329226d5f1a85198d8cda61c389cd9d7232
Chris Crocker-White <chriscw@balena.io>
2022-05-03 12:16:54 -0700

    Add gitignore and wordlists
    Change-type: patch
    Signed-off-by: Chris Crocker-White <chriscw@balena.io>

--- sentiment-analysis/analyse.js
+++ sentiment-analysis/analyse.js
@@ -1,14 +1,18 @@
 const fs = require('fs');
 const vader = require('vader-sentiment');
-
+var Sentiment = require('sentiment');
+var sentiment = new Sentiment();
 
 try {
     // assume repo readme is at /input/input.md
-    const data = fs.readFileSync('/input/.md', 'utf8');
+    const data = fs.readFileSync('/input/input.md', 'utf8');
     var intensity = vader.SentimentIntensityAnalyzer.polarity_scores(data);
+    var result = sentiment.analyze(data);
     console.dir('Sentiment: ' + intensity.compound);
     try {
       fs.writeFileSync('/output/sentiment.txt', `${intensity.compound}`);
+      fs.writeFileSync('/output/positive.txt', `${result.positive}`);
+      fs.writeFileSync('/output/negative.txt', `${result.negative}`);
       // file written successfully
     } catch (err) {
       console.error(err);
