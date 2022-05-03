50f1a01d084be141fd74aeed8e630afd14366fa1
zoobot <rommims@gmail.com>
2022-05-03 07:47:18 -0700

    download repos

---
+++ gitbrander/download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+[[ -d transformers ]] || git clone https://github.com/huggingface/transformers.git
+[[ -d vqgan-jax ]] || git clone https://github.com/patil-suraj/vqgan-jax.git
+[[ -d dalle-mini ]] || git clone https://github.com/borisdayma/dalle-mini.git
