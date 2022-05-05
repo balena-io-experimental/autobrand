fc678c29ff0f403d3facb82a3782b47e7fee5b44
Rahul Thakoor <rahul.thakoor@gmail.com>
2022-05-05 12:45:15 +0400

    UI -> readme-getter -> sentiment-analysis

--- hack.py
+++ hack.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-
-
-import time
+import os, json
 
 app = Flask(__name__)
 
@@ -14,10 +12,63 @@ def index():
 def run():
     if request.method == 'POST':
       repo = request.form['repo_url']
-      # clone repo
-      # get readme
-      # start transformer chain
-      print(repo)
 
-      time.sleep(15)
+      # write input.json to readme-getter 
+
+      readme_getter_input_contract_data = {
+        "input": {
+          "contract": {
+                  "type": "repourl",
+                  "data": {
+                      "url": repo
+                  }
+              },
+          "transformerContract": {
+                  "type": "transformer",
+                  "handle": "repourl2readme"
+              },
+              "artifactPath": "foobar"
+        }
+    }
+
+      with open('./readme-getter/input.json', 'w') as f:
+        f.write(json.dumps(readme_getter_input_contract_data))
+
+      # keep track of project root dir
+      root = os.getcwd()
+
+      print("Executing readme-getter ...")
+
+      # change to transformer directory so docker runs in proper context
+      os.chdir('./readme-getter')
+
+      os.system("./run.sh")
+
+      print("readme-getter done ...")
+
+      os.chdir(root)
+
+      # get output.json from readme-getter and feed to sentiment-analysis
+
+      readme_getter_file = json.load(open('./readme-getter/output/output.json'))
+
+      readme_getter_contract = readme_getter_file['results'][0]['contract']
+
+      # write readme text 
+
+      with open('./sentiment-analysis/input/input.md', 'w') as f:
+        f.write(readme_getter_contract['data']['text'])
+
+      # start sentiment-analysis
+      print("Executing sentiment analysis ...")
+
+      # change to transformer directory so docker runs in proper context
+      os.chdir('./sentiment-analysis')
+
+      os.system("./run.sh")
+
+      print("sentiment analysis done ...")
+
+      os.chdir(root)
+
       return redirect(url_for('index'))
