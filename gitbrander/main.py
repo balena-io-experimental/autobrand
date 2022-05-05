5bb661ba36577d10211e728073fe9aa3dcb25717
zoobot <rommims@gmail.com>
2022-05-05 09:04:14 -0700

    simplified image

--- gitbrander/main.py
+++ gitbrander/main.py
@@ -1,8 +1,9 @@
-from PIL import Image, ImageEnhance
+from PIL import Image, ImageEnhance, ImageFilter, ImageOps
 import os
 import json
 import requests
 import base64
+import cv2 
 # from docarray import Document
 
 def inputTransformer(input_text=None):
@@ -43,26 +44,54 @@ def outputTransformer(image=None):
         f.write(image)
 
     # image.save("output/logo1.png", "PNG")
-    img1 = Image.open(full_output_path)
-    # im3 = ImageEnhance.Color(im)
-    img2 = ImageEnhance.Contrast(img1).enhance(0.0)
-    img3 = ImageEnhance.Brightness(img2).enhance(2.0)
+    im = Image.open(full_output_path).convert("L")
+
     
+    # im = im.convert("1")
+    # im = ImageEnhance.Color(im).enhance(0.0)
+    # im = im.filter(ImageFilter.GaussianBlur(radius = .5))
+    # im = cv2.cvtColor(im, cv2.COLOR_BGR2HSV )
+    # im.save("output/logo2.png", "PNG")
 
     # Convert to transparent
-    rgba = img.convert("RGBA")
-    datas = rgba.getdata()
+    # rgba = im.convert("RGBA")
+    # im = im.convert("RGB")
+        # im = ImageEnhance.Color(im).enhance(0.0)
+
+    im = ImageEnhance.Brightness(im).enhance(1.2)
+    im.save("output/logo3.png", "PNG")
+    # im.save("output/logo4.png", "PNG")
+    # im = ImageEnhance.Color(im).enhance(5.0)
+    # im.save("output/logo5.png", "PNG")
+    # im = ImageOps.posterize(im, 2)
     
-    newData = []
-    for item in datas:
-        if item[0] == 255 and item[1] == 255 and item[2] == 255:  # finding white color by its RGB value
-            # storing a transparent value when we find a white color
-            newData.append((255, 255, 255, 0))
-        else:
-            newData.append(item)  # other colours remain unchanged
+    im = ImageOps.colorize(im, black ="blue", white ="white")
+
+    im = ImageEnhance.Contrast(im).enhance(5.0)
+    im.save("output/logo4.png", "PNG")
+    im = ImageOps.autocontrast(im)
+    im.save("output/logo5.png", "PNG")
+    # datas = im.getdata()
+
+    # Matrix = ( 1.1,   0,  0, 0, 
+    #        0,   0.9,  0, 0, 
+    #        0,     0,  1, 0) 
+
+    # # Apply transform and save 
+    # im = im.convert("RGB", Matrix)
+    # im.save("output/logo8.png", "PNG")
+    # newData = []
+    # for item in datas:
+    #     if item[0] <= 200 and item[1] <= 200 and item[2] <= 200:  # findingblack color by its RGB value
+    #         # storing a transparent value when we find black color
+    #         # newData.append((0, 128, 0, 0))
+
+    #         newData.append((0, 128, 0))
+    #     else:
+    #         newData.append(item)  # other colours remain unchanged
     
-    rgba.putdata(newData)
-    rgba.save("output/logo2.png", "PNG")
+    # im.putdata(newData)
+    # im.save("output/logo9.png", "PNG")
 
 
 def setData(prompt, num_images):
@@ -92,7 +121,7 @@ def setHeaders():
 #     return jsons
 
 def fetchColab(prompt, num_images):
-    url = 'https://cold-clocks-wave-35-229-140-35.loca.lt/dalle'
+    url = 'https://chubby-groups-tell-35-189-160-197.loca.lt/dalle'
     payload = setData(prompt, num_images)
     headers = setHeaders()
     response = requests.post(url, json=payload, headers=headers)
@@ -100,6 +129,6 @@ def fetchColab(prompt, num_images):
     return base64.b64decode(image_list[0])
 
 if __name__ == "__main__":
-    prompt = inputTransformer('autobrand logo vector image')
+    prompt = inputTransformer('Green alphabet letters A and B')
     image_one = fetchColab(prompt, 1)
     image = outputTransformer(image_one)
